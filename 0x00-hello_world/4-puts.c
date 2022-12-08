6857b545789e6eab48d0f8c6cf61eb8811408aa0
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2022-12-08 15:17:59 -0800

    Printing using put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- *
- * Description: A c program that prints with puts function
- *
- * Return: Always 0 
+ * * main - Receives no arguement value
+ * *
+ * * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
