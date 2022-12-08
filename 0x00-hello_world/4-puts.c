5946c23794deecc178f7ca9df14b12784c80d4a4
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2022-12-08 12:55:45 -0800

    Printing with put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A c program that prints with c function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	Return (0);
+}
