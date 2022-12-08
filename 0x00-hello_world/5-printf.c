caf4c94c7bc6b0ec6b65a66eb169a23e4d321cd3
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2022-12-08 15:49:28 -0800

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * * main - Recieves no arguement value
- * * 
- * * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n", grammer);
+	printf("with proper grammer, but the outcome is a piece of art,\n",);
 	return (0);
 }
