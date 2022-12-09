0e32216f54155e4c8ba9c642e31f47be6229adc3
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2022-12-09 12:20:24 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all letters of base 16
+ * in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char alpha;
+
+	for (num = 0; num <= 10; num++)
+		putchar((num % 10) + '0');
+
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+
+}
