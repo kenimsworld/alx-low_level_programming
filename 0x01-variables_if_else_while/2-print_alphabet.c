6537ac3173981265e7e493e0feefaaa1b2546d45
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2022-12-09 10:20:09 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower cases.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ken;
+
+	for (ken = 'a'; ken <= 'z'; ken++)
+	{
+		putchar(ken);
+	}
+	purchar('\n');
+	return (0);
+}
