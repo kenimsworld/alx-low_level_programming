3f58bc80ed1d3ea94b0aa81720b21b3a1a51486a
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2022-12-09 12:38:40 -0800

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,8 +10,8 @@ int main(void)
 	int num;
 	char alpha;
 
-	for (num = 0; num <= 10; num++)
-		putchar(num % 10);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
 	for (alpha = 'a'; alpha <= 'f'; alpha++)
 		putchar(alpha);
