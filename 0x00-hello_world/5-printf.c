9aad56f8136559b1f0888ab9610d3ff559e387b6
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2022-12-09 02:29:32 -0800

    printing with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print a c program using printf
  *
- * Return: Always 0 (Success)
+ * Return: 0
 */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return  (0);
 }
