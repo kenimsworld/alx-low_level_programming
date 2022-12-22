2d74f4c2fa2efb901b78c56b8996e1449593feb8
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2022-12-22 07:08:46 -0800

    A function that copies a string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - function to append some charx
+ * @src: first param
+ * @dest: second param
+ * @n: 3rd param
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, int n);
+	return (dest);
+}
