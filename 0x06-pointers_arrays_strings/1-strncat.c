db1dcaac995df99fc39ab80cc05a1d01f827c9d7
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2022-12-22 07:20:48 -0800

    Task 1: A function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,26 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _strncat - function to append some charx
- * @src: first param
- * @dest: second param
- * @n: 3rd param
- * Return: string
+ * _strncat - concatenates two strings
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, int n);
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
 	return (dest);
 }
