5da425214997a41b7d655b067ca3d1097b35c23a
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-01-12 06:30:11 -0800

    a function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,9 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * malloc_checked - cause normal process termination with a status value of 98
+ * *malloc_checked - cause normal process termination with a status
+ * value of 98
  *
  * @b: allocated memory
  *
@@ -12,8 +14,10 @@ void *malloc_checked(unsigned int b)
 	void *i;
 
 	i = malloc(b);
+
 	if (i == NULL)
 		exit(98);
+
 	return (i);
 
 }
